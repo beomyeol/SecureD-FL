613d355dbf55a9b4ff7e88fb6539b406cbc1f2f1
Beomyeol Jeon <beomyeol.jeon@nokia.com>
2019-07-18 15:32:56 -0400

    Update experiment run script

--- experiments/run.sh
+++ experiments/run.sh
@@ -6,6 +6,7 @@ EXPERIMENT_DIR="$HOME_DIR/experiments"
 DATA_ROOT_DIR="$HOME_DIR/data"
 PYTHON="python3"
 
+NUM_WORKERS=10
 MODEL="lenet"
 DATASET_DIR="$DATA_ROOT_DIR/femnist"
 EPOCHS=10
@@ -15,11 +16,12 @@ BATCH_SIZE=64
 
 function get_cmd {
   local CMD="$PYTHON $ROOT_DIR/sequential_simulation_main.py \
-    --dataset_dir=$DATASET_DIR
-    --model=$MODEL
-    --epochs=$EPOCHS
-    --local_epochs=$LOCAL_EPOCHS
-    --log_every_n_etsps=$LOG_EVERY_N_STEPS
+    --dataset_dir=$DATASET_DIR \
+    --model=$MODEL \
+    --epochs=$EPOCHS \
+    --local_epochs=$LOCAL_EPOCHS \
+    --log_every_n_steps=$LOG_EVERY_N_STEPS \
+    --num_workers=$NUM_WORKERS \
     --validation_period=1"
   echo $CMD
 }
@@ -28,6 +30,14 @@ function run {
   local CMD=$1
   local LOG_PATH=$2
 
+  local LOG_PATH_DIR=$(dirname $LOG_PATH)
+
+  if ! [ -d $LOG_PATH_DIR ]; then
+    mkdir -p $LOG_PATH_DIR
+  fi
+
   echo $CMD > $LOG_PATH
   $CMD |& tee -a $LOG_PATH
 }
+
+run "$(get_cmd)" "$EXPERIMENT_DIR/femnist/run.log"
