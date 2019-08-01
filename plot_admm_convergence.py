ff840772d74e4be6264e5f056cf42c722f69e770
Beomyeol Jeon <beomyeol.jeon@nokia.com>
2019-08-01 14:46:11 -0400

    Support multiple rhos in a single plot

--- plot_admm_convergence.py
+++ plot_admm_convergence.py
@@ -1,5 +1,6 @@
 from __future__ import absolute_import, division, print_function
 
+import copy
 import torch
 import numpy as np
 import matplotlib.pyplot as plt
@@ -22,9 +23,12 @@ def get_value(state_dict):
     return state_dict['weight'].item()
 
 
+COLORS = ['b', 'g', 'r', 'c', 'm', 'y', 'k', 'w']
+
+
 def main():
     num_trials = 1
-    num_data = 2
+    num_data = 3
     device = torch.device('cpu')
 
     weights = [1/num_data] * num_data
@@ -34,48 +38,73 @@ def main():
         models = generate_models(num_data, device)
         workers = [ADMMWorker(model, device) for model in models]
         mean = fedavg(models, weights=weights)
-        aggregator = ADMMAggregator(workers, weights,
-                                    max_iter=max_iter, threshold=0.0, lr=2,
-                                    decay_rate=None, decay_period=None)
-
-        xs_history_dict = {i: [] for i in range(num_data)}
-        zs_history_dict = {i: [] for i in range(num_data)}
-        lambda_history_dict = {i: [] for i in range(num_data)}
-
-        for _ in range(max_iter):
-            aggregator.run_step()
-            for i in range(num_data):
-                worker = aggregator.admm_workers[i]
-                xs_history_dict[i].append(worker.xs[0].item())
-                zs_history_dict[i].append(get_value(worker.zs))
-                lambda_history_dict[i].append(worker.lambdas[0].item())
+        aggregator_base = ADMMAggregator(workers, weights,
+                                         max_iter=max_iter, threshold=0.0,
+                                         lr=None,
+                                         decay_rate=None, decay_period=None)
 
         n_rows = 3
         n_cols = num_data
-
         xs = list(range(max_iter))
 
-        for i in range(num_data):
-            plt.subplot(n_rows, n_cols, i + 1)
-            # plot zs
-            plt.hlines(get_value(mean), 0, max_iter-1)
-            plt.plot(xs, zs_history_dict[i])
-            if i == 0:
-                plt.ylabel('z')
+        zs_axes = [plt.subplot(n_rows, n_cols, i) for i in range(1, num_data+1)]
+        xs_axes = [plt.subplot(n_rows, n_cols, n_cols + i)
+                   for i in range(1, num_data+1)]
+        lambdas_axes = [plt.subplot(n_rows, n_cols, 2 * n_cols + i)
+                        for i in range(1, num_data+1)]
 
-            plt.subplot(n_rows, n_cols, n_cols + i + 1)
-            # plot xs
-            w = get_value(models[i].state_dict())
-            plt.hlines(w, 0, max_iter-1)
-            plt.plot(xs, xs_history_dict[i])
-            if i == 0:
-                plt.ylabel('x')
+        lrs = [4, 2, 1, 0.5]
+        colors = COLORS[:len(lrs)]
 
-            plt.subplot(n_rows, n_cols, 2 * n_cols + i + 1)
-            # plot lambdas
-            plt.plot(xs, lambda_history_dict[i])
-            if i == 0:
-                plt.ylabel('lambda')
+        print(get_value(mean))
+
+        for lr, color in zip(lrs, colors):
+            aggregator = copy.deepcopy(aggregator_base)
+            aggregator.lr = lr
+
+            xs_history_dict = {i: [] for i in range(num_data)}
+            zs_history_dict = {i: [] for i in range(num_data)}
+            lambda_history_dict = {i: [] for i in range(num_data)}
+
+            for _ in range(max_iter):
+                for i in range(num_data):
+                    worker = aggregator.admm_workers[i]
+                    lambda_history_dict[i].append(worker.lambdas[0].item())
+
+                aggregator.run_step()
+                for i in range(num_data):
+                    worker = aggregator.admm_workers[i]
+                    xs_history_dict[i].append(worker.xs[0].item())
+                    zs_history_dict[i].append(get_value(worker.zs))
+
+            for i in range(num_data):
+                plt.subplot(zs_axes[i])
+                # plot zs
+                plt.hlines(get_value(mean), 0, max_iter-1)
+                plt.plot(xs, zs_history_dict[i],
+                         color=color, label='rho=%s' % str(lr))
+                if i == 0:
+                    plt.ylabel('z')
+                plt.legend()
+
+                plt.subplot(xs_axes[i])
+                # plot xs
+                w = get_value(models[i].state_dict())
+                plt.hlines(w, 0, max_iter-1)
+                plt.plot(xs, xs_history_dict[i],
+                         color=color, label='rho=%s' % str(lr))
+
+                if i == 0:
+                    plt.ylabel('x')
+                plt.legend()
+
+                plt.subplot(lambdas_axes[i])
+                # plot lambdas
+                plt.plot(xs, lambda_history_dict[i],
+                         color=color, label='rho=%s' % str(lr))
+                if i == 0:
+                    plt.ylabel('lambda')
+                plt.legend()
 
         plt.show()
 
