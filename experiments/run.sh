b3e79c6fce10ba3b0697889aa102ae5d0727b037
Beomyeol Jeon <beomyeol.jeon@nokia.com>
2019-08-08 17:29:45 +0000

    Use different seeds for different trials

--- experiments/run.sh
+++ experiments/run.sh
@@ -6,6 +6,8 @@ ROOT_DIR="${HOME}/federated"
 EXPERIMENT_DIR="${HOME}/experiments"
 DATA_ROOT_DIR="${HOME}/data"
 
+NUM_GPUS=4
+
 # LeNet
 MODEL="lenet"
 DATASET_DIR="$DATA_ROOT_DIR/femnist"
@@ -22,13 +24,13 @@ BATCH_SIZE=32
 EPOCHS=10
 LOCAL_EPOCHS=10
 LOG_DIR="$(get_log_dir)/fedavg"
-run_with_ckpt_gpus "$(get_cmd)" "${LOG_DIR}"
+run_with_ckpt_gpus "$(get_cmd)" "${LOG_DIR}" "${NUM_GPUS}"
 
 # local only
 EPOCHS=1
 LOCAL_EPOCHS=100
 LOG_DIR="$(get_log_dir)/local_only"
-#run_with_ckpt_gpus "$(get_cmd)" "${LOG_DIR}"
+#run_with_ckpt_gpus "$(get_cmd)" "${LOG_DIR}" "${NUM_GPUS}"
 
 # ADMM
 EPOCHS=10
@@ -39,5 +41,5 @@ ADMM_DECAY_RATE=1
 ADMM_DECAY_PERIOD=12
 ADMM_THRESHOLD=1e-6
 LOG_DIR="$(get_log_dir)/admm_${ADMM_LR}_${ADMM_DECAY_RATE}_${ADMM_DECAY_PERIOD}_${ADMM_THRESHOLD}"
-#run_with_ckpt_gpus "$(get_cmd) $(get_admm_args)" "${LOG_DIR}"
+#run_with_ckpt_gpus "$(get_cmd) $(get_admm_args)" "${LOG_DIR}" "${NUM_GPUS}"
 
