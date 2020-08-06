d58e982bd959c4c9fc31ff7776a4730531a3ac43
Beomyeol Jeon <bj2@illinois.edu>
2020-08-05 21:51:35 -0500

    Update plot_summary

--- scripts/plot_summary.py
+++ scripts/plot_summary.py
@@ -1,8 +1,14 @@
 import argparse
 import matplotlib.pyplot as plt
+import numpy as np
 
 from tensorboard.backend.event_processing import event_accumulator
 
+_LABELS = {
+    'train_loss': 'Loss',
+    'train_accuracy': 'Accuracy',
+    'validation_accuracy (after aggr.)': 'Accuracy',
+}
 
 def load_events(path, name):
     ea = event_accumulator.EventAccumulator(path)
@@ -20,23 +26,45 @@ def main():
                         help='Label(s) on the plot')
     parser.add_argument('--name', required=True, help='Scalar name to read')
     parser.add_argument('--output', help='Output name.')
+    parser.add_argument('--yticks',
+                        help='comma separate range (start,end,step)')
 
     args = parser.parse_args()
 
-    markers = ["x", "+", "."]
-    colors = ["r", "b", "g"]
+    markers = [".", "x", "+"]
+    colors = ["g", "r", "b"]
+
+    fontsize = 14
 
     for idx, (logdir, label) in enumerate(zip(*[args.logdir, args.label])):
         print('label={}, logdir={}'.format(label, logdir))
 
-        events = load_events(logdir, args.name)
+        name = args.name
+        if 'local_only' in logdir and '(after aggr.)' in name:
+            name = name[:name.find('(after aggr.)')].rstrip()
+
+        events = load_events(logdir, name)
 
         steps, values = tuple(zip(*events))
 
         plt.plot(steps, values, label=label,
                  marker=markers[idx], color=colors[idx])
 
-    plt.legend()
+    plt.xlabel('Round', fontsize=fontsize)
+    plt.ylabel(_LABELS[args.name], fontsize=fontsize)
+    plt.xticks(fontsize=fontsize)
+    if args.yticks:
+        start, end, step = map(lambda x: float(x), args.yticks.split(','))
+        plt.yticks(np.arange(start, end, step), fontsize=fontsize)
+    else:
+        plt.yticks(fontsize=fontsize)
+    plt.tight_layout()
+
+    if 'accuracy' in args.name:
+        legend_loc = 'lower right'
+    else:
+        legend_loc = 'best'
+    plt.legend(loc=legend_loc, fontsize=fontsize)
 
     if args.output:
         print('Saving the plot to {}...'.format(args.output))
