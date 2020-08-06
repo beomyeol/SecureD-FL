1c46522c1b9995e2e916181f5b49fb531b445e38
Beomyeol Jeon <bj2@illinois.edu>
2020-08-05 21:51:21 -0500

    Update parse_summary to support --key

--- scripts/parse_summary.py
+++ scripts/parse_summary.py
@@ -5,12 +5,18 @@ from tensorboard.backend.event_processing import event_accumulator
 def main():
     parser = argparse.ArgumentParser()
     parser.add_argument('--logdir', required=True, help='TF summary log dir')
-    parser.add_argument('--name', required=True, help='Scalar names to read')
+    parser.add_argument('--keys', action='store_true', help='View keys')
+    parser.add_argument('--name', help='Scalar names to read')
 
     args = parser.parse_args()
 
     ea = event_accumulator.EventAccumulator(args.logdir)
     ea.Reload()
+
+    if args.keys:
+        for key in ea.scalars.Keys():
+            print(key)
+        return
 
     events = ea.Scalars(args.name)
 
